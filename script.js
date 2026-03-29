14f0d5113bf12dff2facc26bf52d234d4902c387
Abdeljalil-Khazri <abdeljalil.kh111@gmail.com>
2026-03-29 18:43:23 +0200

    feat: add product button interaction

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ const menu = document.querySelector("nav");
 menu.addEventListener("click", () => {
   console.log("Navbar clicked");
 });
+<button id="buyBtn">Buy Now</button>
+const button = document.getElementById("buyBtn");
+
+button.addEventListener("click", () => {
+  alert("Product added to cart");
+});
