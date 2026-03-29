5dce3f64406aca5c2569f3e7442c8ffc2c2167a0
Abdeljalil-Khazri <abdeljalil.kh111@gmail.com>
2026-03-29 18:42:42 +0200

    feat: add navbar interaction

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 console.log("SWEVEN website loaded");
 <script src="../js/script.js"></script>
+const menu = document.querySelector("nav");
+
+menu.addEventListener("click", () => {
+  console.log("Navbar clicked");
+});
